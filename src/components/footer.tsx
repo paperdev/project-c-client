d872c30206ce81224861bfc97ad768e809b81cb8
Kevin <paperdev0@gmail.com>
2023-11-22 23:18:26 +0900

    bug fixed

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,18 +1,8 @@
 'use client';
 
-import { usePathname } from 'next/navigation';
-import ComponentChatInput from '@/components/(chat)/chatInput';
-
-const URL_CHAT = '/chat';
-
 export default function Footer() {
-  const currentUrl = usePathname();
-
   return (
     <>
-      {
-        currentUrl === URL_CHAT && <ComponentChatInput />
-      }
     </>
   );
 }
